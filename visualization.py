c23381ff87a7abaece3385ba0e90331d128bac61
testing-user-prog <muhammadzaidbinsaad@gmail.com>
2026-02-08 19:41:50 +0500

    Add files via upload
    divided dataLoader implementation into functions

--- visualization.py
+++ visualization.py
@@ -1,35 +1,46 @@
 import matplotlib.pyplot as plt
 
-def visualizeDataofRegion(gdp):
+
+def horizontalbarchart(gdp):
+    
     names = [list(row.keys())[0] for row in gdp]
     values = [list(row.values())[0] for row in gdp]
 
-    plt.figure(figsize=(10, 12))
     plt.barh(names, values, color='skyblue')
-
     plt.xlabel('GDP (USD)')
     plt.ylabel('Country')
     plt.title('GDP Comparison by country')
 
     # This puts the first item in your list at the top instead of the bottom
     plt.gca().invert_yaxis() 
-
-    plt.tight_layout()
     plt.show()
 
-def visualizeDataofCountry(gdp):
+def barchart(gdp):
     names = [list(row.keys())[0] for row in gdp]
     values = [list(row.values())[0] for row in gdp]
-
-    plt.figure(figsize=(10, 12))
     plt.bar(names, values, color='skyblue')
-
     plt.ylabel('GDP (USD)')
     plt.xlabel('Year')
     plt.title('GDP Comparison by year')
-
-    plt.tight_layout()
     plt.show()
+
+def scatterplot(gdp):
+   names=[list(row.keys())[0] for row in gdp]
+   values=[list(row.values())[0] for row in gdp]
+   plt.title('GDP Comparison by year')
+   plt.xlabel("Years")
+   plt.ylabel("gdp")
+   plt.scatter(names,values)
+   plt.show()
+
+
+
+def visualizeDataofRegion(gdp):
+    horizontalbarchart(gdp)
+
+def visualizeDataofCountry(gdp):
+   barchart(gdp)
+   scatterplot(gdp)
 
 
 
