3d566358af544bfda60f70068a353d210107b778
testing-user-prog <muhammadzaidbinsaad@gmail.com>
2026-02-08 16:53:42 +0500

    Add files via upload
    added dataprocessor which calculate sum and gdp

--- dataprocessor.py
+++ dataprocessor.py
@@ -1,40 +1,16 @@
-import dataloader
-import validcontinent
-import json
-from functools import reduce
-askregion=None
-askcountry=None
-time=None
-outputwindow=None
-operation=None
-with open("config.json", "r") as file:
-    data = json.load(file)
-    askregion=data["region"]
-    time=str(data["year"])
-    outputwindow=(data["output"])
-    operation=(data["operation"])
-regiondata=list(filter(lambda x: validcontinent.isvalidregion(x, askregion), dataloader.yearlygdp))
-index=None
-timedata=None;
-timedata_=None;
-sum=None;
-if time in dataloader.years and regiondata:
-    index=dataloader.years.index(time)+1;
+from functools import  reduce
 
-    timedata_=list(map(lambda x:validcontinent.atvalidtime(x,index),regiondata))
-    timedata=list(filter(lambda t:validcontinent.removenill(t),timedata_))
-    if not timedata:
-        timedata=[["No country data is entered","0"]]
-        sum=["0"];
-    else:
+def doSum(x, row):
+    gdp_value = list(row.values())[0]
+    return x + gdp_value
 
-        sum=reduce(lambda acc,x:validcontinent.findsum(acc,x),timedata,["0"])
-        if operation=="average" and len(timedata):
-             sum[0]=str(float(sum[0])/len(timedata))
+def doOperation(config,data):   
+    sum= reduce(doSum,data,0.0) #function , iteratable,initalizer 
+    if(config["operation"]=='sum'):
+        return sum 
+    elif config["operation"]=='average':
+        return sum/len(data)
+    else :
+        return None
 
-elif not regiondata:
-    timedata=[["No region exists with this name","0"]]
-    sum=["0"];
-else:
-    timedata=[["No time exists","0"]]
-    sum=["0"];
+
