89608b3927fae59ada7860c6fd5fd29174a6bb51
Muhammad Abubakar Khan <abubakarrrkhan123@gmail.com>
2026-02-08 20:42:09 +0500

    Add files via upload
    added pie chart

--- visualization.py
+++ visualization.py
@@ -5,15 +5,29 @@ def horizontalbarchart(gdp):
     
     names = [list(row.keys())[0] for row in gdp]
     values = [list(row.values())[0] for row in gdp]
-
+    plt.figure(figsize=(10, 12))
     plt.barh(names, values, color='skyblue')
     plt.xlabel('GDP (USD)')
     plt.ylabel('Country')
     plt.title('GDP Comparison by country')
 
     # This puts the first item in your list at the top instead of the bottom
-    plt.gca().invert_yaxis() 
+    plt.gca().invert_yaxis()
+
+    plt.tight_layout()
     plt.show()
+
+def piechart(gdp):
+    # Sort the data and take only the top 10
+    sorted_gdp = sorted(gdp, key=lambda x: list(x.values())[0], reverse=True)[:10]
+    names = [list(row.keys())[0] for row in sorted_gdp]
+    values = [list(row.values())[0] for row in sorted_gdp]
+    plt.figure(figsize=(10, 7))
+    plt.pie(values, labels=names, autopct='%1.1f%%', startangle=140)
+    plt.title('Top 10 GDP Shares in the Region')
+    #plt.axis('equal')
+    plt.show()
+
 
 def barchart(gdp):
     names = [list(row.keys())[0] for row in gdp]
@@ -37,7 +51,7 @@ def scatterplot(gdp):
 
 def visualizeDataofRegion(gdp):
     horizontalbarchart(gdp)
-
+    piechart(gdp)
 def visualizeDataofCountry(gdp):
    barchart(gdp)
    scatterplot(gdp)
