8df32568bc8f242e8d266b8aac126ed392db1231
Muhammad Abubakar Khan <abubakarrrkhan123@gmail.com>
2026-02-08 23:39:05 +0500

    made changes in dashboard to import json and csv file

--- dashboard.py
+++ dashboard.py
@@ -5,7 +5,6 @@ import visualization
 config=dataloader.loadConfig('config.json')
 
 cleaned=dataloader.clean('gdp.csv')#cleaned is list of dictionary(convert dataType of string(gdp value) to float and empty gdp value to 0.0)
-
 regionList=dataloader.get_countries(config,cleaned)# short list the input countries
 
 if len(regionList)==0:# mean user entered region/country not available
@@ -13,7 +12,6 @@ if len(regionList)==0:# mean user entered region/country not available
 
 gdpList=dataloader.get_gdp_list(config,regionList)# get a dictionary 
 result = dataprocessor.doOperation(config,gdpList)# apply operation
-
 
 print("=========== GDP DASHBOARD ===========")
 print("The selected records from user data is : ")
