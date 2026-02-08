7f9a36bce6564606a9cfe33d68a59c6d60ece9aa
Muhammad Abubakar Khan <abubakarrrkhan123@gmail.com>
2026-02-08 23:44:45 +0500

    added some changes in dashboard to test opening csv and json file

--- dashboard.py
+++ dashboard.py
@@ -2,9 +2,20 @@ import dataloader
 import dataprocessor
 import visualization
 
-config=dataloader.loadConfig('config.json')
 
-cleaned=dataloader.clean('gdp.csv')#cleaned is list of dictionary(convert dataType of string(gdp value) to float and empty gdp value to 0.0)
+import os
+
+# Get the directory where the script is located
+base_path = os.path.dirname(__file__)
+
+# Join it with your filename
+json_path = os.path.join(base_path, 'config.json')
+csv_path = os.path.join(base_path, 'gdp.csv')
+
+
+config=dataloader.loadConfig(json_path)
+
+cleaned=dataloader.clean(csv_path)#cleaned is list of dictionary(convert dataType of string(gdp value) to float and empty gdp value to 0.0)
 regionList=dataloader.get_countries(config,cleaned)# short list the input countries
 
 if len(regionList)==0:# mean user entered region/country not available
