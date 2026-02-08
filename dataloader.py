ea79b315ff231fce8755c72471b42672b45f0f87
testing-user-prog <muhammadzaidbinsaad@gmail.com>
2026-02-08 16:52:06 +0500

    Add files via upload
    divided dataLoader implementation into functions

--- dataloader.py
+++ dataloader.py
@@ -1,36 +1,57 @@
-import csv
+import json 
+import csv 
 
-yearlygdp=[];
-years=[];  
-  
-             
+# r mean for reading 
+
+def loadConfig(file='config.json'):#giving default parameter of config.json
+    try:
+        with open (file,'r') as userdata:
+            userInput= json.load(userdata)#a dict of user input(file)
+            if userInput["region"]=="":
+                raise ValueError("Empty region in file")
+            if userInput["year"]<0 or userInput["year"]=="":
+                raise ValueError("Negative or empty year in file")
+            if userInput["operation"]!='sum' and userInput["operation"]!='average':
+                raise ValueError("Invalid operation")
+            if userInput["output"]!='dashboard':
+                raise ValueError("Invalid output window")
+            return userInput
+    except FileNotFoundError:
+        print("File not found")
+        return None  
+
+
+def clean(file='gdp.csv'):  #python treat values as string when DictReader, change it to float , in case of empty value place 0.0 
+    try :
+        with open(file,'r') as regionsData:
+            reader=csv.DictReader(regionsData)
+            cleaned = map(lambda row: {
+                key: (float(value) if value.strip() else 0.0) if key.isdigit() else value 
+                for key, value in row.items()
+            }, reader)
+            return list(cleaned)
+    except FileNotFoundError:
+        print("File not found ")
+        return None
 
 
 
-with open("gdp.csv", "r") as file:
-    reader = csv.reader(file)
-    i=0;
-    rownum=-1;
+
+def findrow(row,config):
+    if  row["Continent"]==config["region"] or row["Country Name"]==config["region"]: 
+        return True
+    else: 
+        return False 
+
+
+def get_countries(config,cleanedData):#cleanedData is list of dictionary
+        try:
+            return list(filter(lambda row: findrow(row, config), cleanedData))#make list of dictionary
+        except FileNotFoundError:
+            print("File not found")
+            return None
 
 
 
 
-    for row in reader:
-        
-        if i < 1:
-           i+=1;
-           for i in range(4,69,1):
-               years.append(str(row[i]));
-              
-           #col 0 in the years array has the data starring from col 4 & row1 of the spreadsheet
-           
-           continue
-        rownum=rownum+1;
-        
-        yearlygdp.append([])
-        
-        yearlygdp[rownum].append(str(row[0]))
-       
-        for i in range(4,70,1):
-             yearlygdp[rownum].append(str(row[i]));
-    
+
